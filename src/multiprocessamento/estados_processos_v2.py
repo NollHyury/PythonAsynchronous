f20cfa5db0cd127108248ae4564f9b3b4bcb81cf
hyury.noll <hyury.noll@statkraft.com>
2022-10-11 13:24:35 -0300

    estados e sincronização de processos

--- src/multiprocessamento/estados_processos_v2.py
+++ src/multiprocessamento/estados_processos_v2.py
@@ -1,37 +1,38 @@
+import ctypes
 import multiprocessing
 import time
 
 
 def funcao1(val, stat):
-    if stat:
-        res = val + 10
-        stat = False
+    if stat.value:
+        res = val.value + 10
+        stat.value = False
 
     else:
-        res = val + 20
-        val = 200
-        stat = True
+        res = val.value + 20
+        val.value = 200
+        stat.value = True
 
     print(f'O resultado da função 1 é {res}')
     time.sleep(0.001)
 
 
 def funcao2(val, stat):
-    if stat:
-        res = val + 30
-        stat = False
+    if stat.value:
+        res = val.value + 30
+        stat.value = False
     else:
-        res = val + 40
-        val = 400
-        stat = True
+        res = val.value + 40
+        val.value = 400
+        stat.value = True
 
     print(f'O resultado da função 2 é {res}')
     time.sleep(0.001)
 
 
 def main():
-    valor = 100
-    status = False
+    valor = multiprocessing.Value('i', 100)
+    status = multiprocessing.Value(ctypes.c_bool, False)
 
     p1 = multiprocessing.Process(target=funcao1, args=(valor, status))
     p2 = multiprocessing.Process(target=funcao2, args=(valor, status))
